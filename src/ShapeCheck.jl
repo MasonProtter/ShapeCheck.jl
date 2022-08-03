e8e45bf4be83065f4a5f64a13a7a97051deeaf8a
Mason Protter <mason.protter@icloud.com>
2022-08-03 15:12:09 -0600

    fixes and enhancements for default and keyword arguments

--- src/ShapeCheck.jl
+++ src/ShapeCheck.jl
@@ -4,37 +4,18 @@ using ExprTools, MLStyle
 
 export @shapechecked
 
-call(f) = f()
-
 macro shapechecked(fdef)
     d = splitdef(fdef)
-    shape_decls = Expr[]
-    shape_names = Symbol[]
-    new_args = map(get!(d, :args, [])) do arg
-        @match arg begin
-            :($x :: $T[$(shapes...)]) => begin
-                for (i, shape) ∈ enumerate(shapes)
-                    if shape == :_
-                        nothing
-                    elseif shape ∈ shape_names || shape isa Expr
-                        # This branch catches cases where either a shape name is repeated, or an expression is used.
-                        # For repeated shape names, we assert that the new dim matches the earlier one.
-                        # For expressions, we assert that it evaluates to true.
-                        # e.g. someone might write f(x::T[a], y::U[a+1])
-                        push!(shape_decls, :($isequal($shape, $size($x, $i))
-                                             || $throw($DimensionMismatch("Shape $($shape) has mismatched sizes"))))
-                    else
-                        push!(shape_names, shape)
-                        push!(shape_decls, :($shape = $size($x, $i)))
-                    end 
-                end
-                :($x :: $T)
-            end
-            x => x
-        end
-    end
+    get!(d, :args, [])
+    get!(d, :kwargs, [])
+    get!(d, :rtype, :Any)
+    get!(d, :body, Expr(:block))
+
+    new_args, arg_names, shape_decls, shape_names = extract_shape_info(d[:args])
+    new_kwargs, kwarg_names, _, _ = extract_shape_info!(d[:kwargs], shape_decls, shape_names)
     @gensym result
-    shape_asserts, new_rtype = @match get!(d, :rtype, [Any]) begin
+    
+    shape_asserts, new_rtype = @match d[:rtype] begin
         :($T[$(out_shapes...)]) => (Expr(:block,
                                          (:($size($result, $i) == $shape
                                             || $throw($DimensionMismatch(
@@ -45,17 +26,69 @@ macro shapechecked(fdef)
     end
     new_body = quote
         $(Expr(:block, shape_decls...))
-        $result = $call() do
-            # Doing this in a `call() do` block in order to capture any possible early `return`s.
-            $(d[:body])
-        end
+        # Doing this in a `call(...)` block in order to capture any possible early `return`s. I re-provide the arguments
+        # instead of closing over them because of https://github.com/JuliaLang/julia/issues/15276
+        $result = $call(($(arg_names...),; $(kwarg_names...),) -> $(d[:body]), $(arg_names...); $(kwarg_names...))
         $shape_asserts
         $result
     end
     d[:args] = new_args
+    d[:kwargs] = new_kwargs
     d[:body] = new_body
     d[:rtype] = new_rtype
     combinedef(d) |> esc
+end
+
+call(f, args...; kwargs...) = f(args...; kwargs...)
+
+function process_shapes!(x, shapes, shape_decls, shape_names)
+    for (i, shape) ∈ enumerate(shapes)
+        if shape == :_
+            nothing
+        elseif shape ∈ shape_names || shape isa Expr
+            # This branch catches cases where either a shape name is repeated, or an expression is used.
+            # For repeated shape names, we assert that the new dim matches the earlier one.
+            # For expressions, we assert that it evaluates to true.
+            # e.g. someone might write f(x::T[a], y::U[a+1])
+            push!(shape_decls, :($isequal($shape, $size($x, $i))
+                                 || $throw($DimensionMismatch("Shape $($shape) has mismatched sizes"))))
+        else
+            push!(shape_names, shape)
+            push!(shape_decls, :($shape = $size($x, $i)))
+        end 
+    end
+end
+
+extract_shape_info(args) = extract_shape_info!(args, Expr[], Symbol[])
+function extract_shape_info!(args, shape_decls, shape_names)
+    v = map(args) do arg
+        #Note: if someone gives an argument a default value, e.g. `f(x=1) = x + 1`, then
+        #      that shows up as `Expr(:kw, :x, 1)` in the AST.
+        arg = @match arg begin
+            #Here we take some care to deal with any function arguments named :_, since
+            # we don't want to accidentally use them as an r-value.
+            :( :: $T) || :(_ :: $T)=> :($(gensym(:_)) :: $T)
+            Expr(:kw, :( :: $T), y) || Expr(:kw, :(_ :: $T), y) => Expr(:kw, :($(gensym(:_)) :: $T), y)
+            Expr(:kw, :_, y) => Expr(:kw, gensym(:_), y)
+            :_ => gensym(:_)
+            x => x
+        end
+        @match arg begin
+            :($x :: $T[$(shapes...)]) => begin
+                process_shapes!(x, shapes, shape_decls, shape_names)
+                (:($x :: $T), x)
+            end
+            Expr(:kw, :($x :: $T[$(shapes...)]), val) => begin
+                process_shapes!(x, shapes, shape_decls, shape_names)
+                (Expr(:kw, :($x :: $T), val), x)
+            end
+            Expr(:kw, x, val) => (Expr(:kw, x, val), x)
+            x => (x, x)
+        end
+    end
+    args_typed = map(x -> x[1], v)
+    arg_names  = map(x -> x[2], v)
+    args_typed, arg_names, shape_decls, shape_names
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,34 +1,60 @@
 using Test, ShapeCheck, OffsetArrays
 
-@shapechecked function bad_remove_last(x :: AbstractVector[n]) :: AbstractVector[n-1]
-	x[1:end-1]
-end
 
-@test_throws DimensionMismatch bad_remove_last(OffsetVector([:a, :b, :c, :d, :e], -2:2))
-
-const AA = AbstractArray
-@shapechecked function horizontal_slice(x::AA[_, n], i) :: AA[n]
-    x[i, :]
-end
-
-@test horizontal_slice([:a :b :c
-                        :d :e :f], 2) == [:d, :e, :f]
-
-@shapechecked function my_vcat(x::AA[a, b], y::AA[c, b]) :: AA[a + c, b]
-    vcat(x, y)
-end
-
-@test my_vcat([1 2; 3 4], [5 6]) == [1 2
-                                     3 4
-                                     5 6]
-
-@shapechecked function foo(x::Vector[a], y::Vector[min(a, 3)]) :: Number
-    s = 0.0
-    for i ∈ 1:min(a, 3)
-        s += x[i] * y[i]
+@testset "basic" begin
+    @shapechecked function bad_remove_last(x :: AbstractVector[n]) :: AbstractVector[n-1]
+	    x[1:end-1]
     end
-    s
+
+    @test_throws DimensionMismatch bad_remove_last(OffsetVector([:a, :b, :c, :d, :e], -2:2))
+
+    AA = AbstractArray
+    @shapechecked function horizontal_slice(x::AA[_, n], i) :: AA[n]
+        x[i, :]
+    end
+
+    @test horizontal_slice([:a :b :c
+                            :d :e :f], 2) == [:d, :e, :f]
+
+    @shapechecked function my_vcat(x::AA[a, b], y::AA[c, b]) :: AA[a + c, b]
+        vcat(x, y)
+    end
+
+    @test my_vcat([1 2; 3 4], [5 6]) == [1 2
+                                         3 4
+                                         5 6]
+
+    @shapechecked function foo(x::Vector[a], y::Vector[min(a, 3)]) :: Number
+        s = 0.0
+        for i ∈ 1:min(a, 3)
+            s += x[i] * y[i]
+        end
+        s
+    end
+
+    @test foo([1, 2], [3, 4]) == 11
+    @test foo([1:10;], [1, 1, 1]) == 6
 end
 
-@test foo([1, 2], [3, 4]) == 11
-@test foo([1:10;], [1, 1, 1]) == 6
+@testset "optional and keyword arguments" begin
+    @shapechecked function f(x::Vector[a], y=1)
+        x .+ y
+    end
+    
+    @test f([1, 2, 3]) == [2, 3, 4]
+
+    
+    @shapechecked function g(x, y::Vector[a]=[1,2,3]; z::Any[a]=[1,1,1]) :: Any[a]
+        x .* y .+ z
+    end
+    
+    @test_throws DimensionMismatch g(1; z=[1, 2])
+    @test g(1, [1,2,3]) == [2, 3, 4]
+    @test g(1; z=[1, 1, 1]) == [2, 3, 4]
+
+    @shapechecked function h(_::Any[a], _) :: Any[1]
+        a
+    end
+    @test h(rand(1000), 2) == 1000
+    
+end
